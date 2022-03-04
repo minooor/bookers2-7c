fe7dcf6ab214c4345cfde42f242cf2dbd2e2c5eb
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-03-04 06:38:37 +0000

    [Update] グループ作成

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,17 @@
 class Group < ApplicationRecord
+  
+  has_one_attached :image
   has_many :group_users
   has_many :users, through: :group_users
-  has_one_attached :image
+  belongs_to :owner, class_name: 'User'
 
   validates :name, presence: true
   validates :introduction, presence: true
+  
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
+  
   
 
 end
