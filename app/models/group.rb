77cb387cc4912a95d418106fd6b0594c13cf91dd
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-03-04 03:48:05 +0000

    [Add] グループ作成画面

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,9 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  has_one_attached :image
 
   validates :name, presence: true
   validates :introduction, presence: true
+  
 end
